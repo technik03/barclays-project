20d249d9c8964929bee460837ab05d020a44e477
technik03 <36135249+technik03@users.noreply.github.com>
2018-10-18 19:32:41 -0700

    Update CustomerSeatingAssignment.java

--- barclays-problem3/barclays-project/src/main/java/com/barclays/theater/seating/CustomerSeatingAssignment.java
+++ barclays-problem3/barclays-project/src/main/java/com/barclays/theater/seating/CustomerSeatingAssignment.java
@@ -3,25 +3,32 @@ package com.barclays.theater.seating;
 import java.util.TreeMap;
 import com.barclays.theater.model.Customer;
 import com.barclays.theater.model.CustomerSeatingRequest;
+import com.barclays.theater.resources.TheaterConstants;
 
 /**
  * This class represents seating assignment for a customer.
  *
+ * @author niharika
+ *
  */
 public class CustomerSeatingAssignment {
-  public final static String CANNOT_HANDLE_PARTY_MSG = " Sorry, we can't handle your party.";
-  public final static String SPLIT_PARTY_MSG = " Call to split party.";
-  private int highestSeatSection;
-  private int totalTheaterSeats;
+  private Integer highestSeatSection;
+  private Integer totalTheaterSeats;
+
+  public CustomerSeatingAssignment() {
+    super();
+    // TODO Auto-generated constructor stub
+  }
 
   /**
+   * This will assign seating to customers and handle split orders.
    *
    * @param seatingAndCustomerRequest
    * @param highestSeatSection
    * @param totalTheaterSeats
    */
   public void processSeating(final CustomerSeatingRequest seatingAndCustomerRequest,
-      final int highestSeatSection, final int totalTheaterSeats) {
+      final Integer highestSeatSection, final Integer totalTheaterSeats) {
     this.highestSeatSection = highestSeatSection;
     this.totalTheaterSeats = totalTheaterSeats;
     assignSeatingToCustomers(seatingAndCustomerRequest.getCustomerMap(),
@@ -38,9 +45,10 @@ public class CustomerSeatingAssignment {
    * @param customerMap
    */
   private void handleSplitOrders(final TreeMap<Integer, Customer> customerMap) {
+    // using java 8 feature
     customerMap.forEach((key, customer) -> {
       if (customer.getSeatingNeeded() > highestSeatSection) {
-        customer.setComments(SPLIT_PARTY_MSG);
+        customer.setComments(TheaterConstants.SPLIT_PARTY_MSG);
       }
 
     });
@@ -53,11 +61,11 @@ public class CustomerSeatingAssignment {
    * @param customerMap
    */
   private void handleLargeOrders(final TreeMap<Integer, Customer> customerMap) {
+    // using java 8 feature
     customerMap.forEach((key, customer) -> {
       if (customer.getSeatingNeeded() > totalTheaterSeats) {
-        customer.setComments(CANNOT_HANDLE_PARTY_MSG);
+        customer.setComments(TheaterConstants.CANNOT_HANDLE_PARTY_MSG);
       }
-
     });
   }
 
@@ -68,10 +76,11 @@ public class CustomerSeatingAssignment {
    * @param seatingMatrix
    */
   private void assignSeatingToCustomers(final TreeMap<Integer, Customer> customerMap,
-      final int[][] seatingMatrix) {
+      final Integer[][] seatingMatrix) {
+    // using java 8 feature
     customerMap.forEach((key, customer) -> {
-      customerLoop: for (int row = 1; row < 6; row++) {
-        for (int section = 1; section < 6; section++) {
+      customerLoop: for (int row = 1; row < TheaterConstants.TOTAL_ROW; row++) {
+        for (int section = 1; section < TheaterConstants.TOTAL_ROW; section++) {
           // If exact match and within 3 row
         if ((seatingMatrix[row][section] == customer.getSeatingNeeded() && row < 4)
             && customer.getRowAssigned() == null) {
@@ -83,7 +92,7 @@ public class CustomerSeatingAssignment {
             && customer.getRowAssigned() == null) {
           // Check if higher number is available in front rows
           for (int rowRepeater = 1; rowRepeater < 4; rowRepeater++) {
-            for (int sectionRepeater = 1; sectionRepeater < 6; sectionRepeater++) {
+            for (int sectionRepeater = 1; sectionRepeater < TheaterConstants.TOTAL_ROW; sectionRepeater++) {
               if ((seatingMatrix[rowRepeater][sectionRepeater] > customer.getSeatingNeeded())
                   && customer.getRowAssigned() == null) {
                 customer.setRowAssigned(rowRepeater);
@@ -102,6 +111,4 @@ public class CustomerSeatingAssignment {
     }
   });
   }
-
-
 }
